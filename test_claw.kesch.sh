ff45cb4f1462f00fd9a61b526fa9e1076ca64eeb
Valentin Clement <clementval@gmail.com>
2016-12-07 14:03:26 +0100

    Add branch support

--- test_claw.kesch.sh
+++ test_claw.kesch.sh
@@ -1,11 +1,30 @@
 #!/bin/bash -x
 
+
+CLAW_BRANCH="master"
+
+while [[ $# -gt 1 ]]
+do
+  key="$1"
+
+  case $key in
+    -b|--branch)
+    CLAW_BRANCH="$2"
+    shift
+    ;;
+  esac
+done
+
+echo "CLAW FORTRAN Compiler full tests"
+echo "================================"
+echo "Branch: $CLAW_BRANCH"
+
 TEST_DIR=buildtemp-claw
 INSTALL_DIR=$PWD/$TEST_DIR/install
 rm -rf $TEST_DIR
 mkdir $TEST_DIR
 cd $TEST_DIR
-git clone git@github.com:C2SM-RCM/claw-compiler.git
+git clone -b $CLAW_BRANCH git@github.com:C2SM-RCM/claw-compiler.git
 cd claw-compiler
 git submodule init
 git submodule update --remote
@@ -14,3 +33,4 @@ git submodule update --remote
 FC=mpif90 CC=mpicc CXX=pgc++ cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR .
 #FC=ftn cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DOMNI_CONF_OPTION=--target=Cray-linux-gnu .
 make all transformation test
+
