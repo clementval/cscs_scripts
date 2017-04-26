cfd4e9be1716ef38d4ea079890d78a912c71b6cd
Valentin Clement <clementval@gmail.com>
2017-04-26 09:51:19 +0200

    Initial version of the install script for ANT

---
+++ install_ant.sh
@@ -0,0 +1,68 @@
+#!/bin/bash 
+
+
+#
+# This scripts helps to test install ANT on CSCS system
+#
+
+
+function show_help(){
+  echo "$0 -i <install-path>"
+  echo ""
+  echo "Options:"
+  echo " -i <install-path> Set an install path"
+}
+
+# Define local variable
+ANT_TAR="apache-ant-1.10.1-bin.tar.gz"
+ANT_BINARY="http://www.pirbot.com/mirrors/apache/ant/binaries/${ANT_TAR}"
+ANT_INSTALL_PATH=""
+ANT_EXTRACTED_DIR="apache-ant-1.10.1"
+
+while getopts "hi:" opt; do
+  case "$opt" in
+  h)
+    show_help
+    exit 0
+    ;;
+  i)
+    ANT_INSTALL_PATH=$OPTARG
+    ;;
+  esac
+done
+
+if [ "${ANT_INSTALL_PATH}" == "" ]
+then
+  echo "Install path must be specified"
+  echo ""
+  show_help
+  exit 1
+fi
+
+echo ""
+echo "ANT install information"
+echo "================================"
+echo "- ANT binary path: $ANT_BINARY"
+echo "- Install path: $ANT_INSTALL_PATH"
+echo "================================"
+echo ""
+
+# Download the binary
+wget ${ANT_BINARY}
+
+# Perform install task
+mkdir -p ${ANT_INSTALL_PATH}
+tar -xf ${ANT_TAR} --directory ${ANT_INSTALL_PATH}
+ANT_INSTALL_PATH=${ANT_INSTALL_PATH}/${ANT_EXTRACTED_DIR}
+
+echo ""
+echo "INTSALLATION PROCEDURE:"
+echo "Define ANT_HOME in your .bashrc file as follows:"
+echo "ANT_HOME=${ANT_INSTALL_PATH}"
+echo "Add ${ANT_HOME}/bin to your PATH variable"
+echo ""
+
+${ANT_INSTALL_PATH}/bin/ant -f ${ANT_INSTALL_PATH}/fetch.xml -Ddest=system
+
+rm ${ANT_TAR}
+
