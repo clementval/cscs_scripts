2739469f44d72c84ad677df741abb86045810328
Valentin Clement <clementval@gmail.com>
2017-04-27 14:14:09 +0200

    Remove useless part

--- ecmwf.offline.sh
+++ ecmwf.offline.sh
@@ -1,51 +1,9 @@
 #!/bin/bash
 
-
 #
 # This scripts helps to pack an offline version of the claw-compiler repository
 # to be build on LXG cluster
 #
-
-
-function show_help(){
-  echo "$0 [-b <branch-name>] [-f] [-c gnu|pgi|cray] [-i <install-path>]"
-  echo ""
-  echo "Options:"
-  echo " -b <branch-name>  Specifiy the branch to be tested"
-  echo " -f                Use the forked repository for test"
-  echo " -c <compiler-id>  Define the base compiler to use"
-  echo " -i <install-path> Set an install path"
-}
-
-# Define local variable
-CLAW_BRANCH="master"
-CLAW_MAIN_REPO="git@github.com:C2SM-RCM/claw-compiler.git"
-CLAW_FORK_REPO="git@github.com:clementval/claw-compiler.git"
-CLAW_REPO=$CLAW_MAIN_REPO
-CLAW_TEST_DIR=buildtemp-claw
-CLAW_INSTALL_DIR=$PWD/$CLAW_TEST_DIR/install
-CLAW_BASE_COMPILER="gnu"
-
-while getopts "hfb:c:i:" opt; do
-  case "$opt" in
-  h)
-    show_help
-    exit 0
-    ;;
-  f)
-    CLAW_REPO=$CLAW_FORK_REPO
-    ;;
-  b)
-    CLAW_BRANCH=$OPTARG
-    ;;
-  c)
-    CLAW_BASE_COMPILER=$OPTARG
-    ;;
-  i)
-    CLAW_INSTALL_DIR=$OPTARG
-    ;;
-  esac
-done
 
 COMPUTER=$(hostname)
 
@@ -61,10 +19,8 @@ echo "- Repo: $CLAW_REPO"
 echo "- Branch: $CLAW_BRANCH"
 echo ""
 
-
+# Set up ANT variables
 export ANT_HOME="/home/ms/ec_ext/extvc/install/ant/apache-ant-1.9.9"
-
-#
 export PATH=$PATH:${ANT_HOME}/bin
 
 # Needed to be able to clone repository and resolve ANT dependencies
