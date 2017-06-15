aaaa0b602c62ba0a4cb0658f494e5c2e367b00c8
Valentin Clement <clementval@gmail.com>
2017-06-15 12:25:32 +0200

    Run shellcheck locally

---
+++ run.shellcheck.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+shellcheck ecmwf.offline.ecgate.step1.sh 
+shellcheck ecmwf.offline.lxg.step2.sh
+shellcheck install.ant.sh
+shellcheck jenkins.claw.sh
+shellcheck jenkins.omni-parse.sh
+shellcheck jenkins.omni.sh
+shellcheck test.claw.sh
