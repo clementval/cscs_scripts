937fa883ef1f4edd8b634e841d806d11f3c3eeec
Valentin Clement <clementval@gmail.com>
2017-07-04 12:26:37 +0200

    Add new script

--- run.shellcheck.sh
+++ run.shellcheck.sh
@@ -2,7 +2,8 @@
 
 shellcheck ecmwf/offline.claw_install.step1_ecgate.sh
 shellcheck ecmwf/offline.claw_install.step2_lxg.sh
-shellcheck cscs/jenkins.install_ant.sh
+shellcheck cscs/install.ant.sh
+shellcheck cscs/jenkins.install-ant.sh
 shellcheck cscs/jenkins.claw.sh
 shellcheck cscs/jenkins.omni-parse.sh
 shellcheck cscs/jenkins.omni.sh
