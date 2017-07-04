4cd9f52992548b393951599349088dc22d07ac8a
Valentin Clement <clementval@gmail.com>
2017-07-04 14:51:54 +0200

    Push

--- run.shellcheck.sh
+++ run.shellcheck.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# ECMWFC scripts
 shellcheck ecmwf/offline.claw_install.step1_ecgate.sh
 shellcheck ecmwf/offline.claw_install.step2_lxg.sh
+
+# CSCS scripts
 shellcheck cscs/install.ant.sh
 shellcheck cscs/jenkins.install-ant.sh
 shellcheck cscs/jenkins.claw.sh
